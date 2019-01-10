0c081804fb507da50ce27936b1a35e393610609f
zk <1016029725@qq.com>
2019-01-10 09:26:19 +0800

    zk 2019/1/10

--- src/util/uploadExcel.java
+++ src/util/uploadExcel.java
@@ -1,4 +1,58 @@
 package util;
 
+import bean.teacherBean;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class uploadExcel {
-}
+    public List<teacherBean> readXls(String path) throws IOException {
+        InputStream is = new FileInputStream(path);
+        HSSFWorkbook hssfWorkbook = new HSSFWorkbook(is);
+        teacherBean user = null;
+        List<teacherBean> list = new ArrayList<teacherBean>();
+        // 循环工作表Sheet
+        for (int numSheet = 0; numSheet < hssfWorkbook.getNumberOfSheets(); numSheet++) {
+            HSSFSheet hssfSheet = hssfWorkbook.getSheetAt(numSheet);
+            if (hssfSheet == null) {
+                continue;
+            }
+            // 循环行Row
+            for (int rowNum = 1; rowNum <= hssfSheet.getLastRowNum(); rowNum++) {
+                HSSFRow hssfRow = hssfSheet.getRow(rowNum);
+                if (hssfRow != null) {
+                    user = new teacherBean();
+                    HSSFCell username = hssfRow.getCell(0);
+                    HSSFCell password = hssfRow.getCell(1);
+                    HSSFCell realname = hssfRow.getCell(2);
+                    HSSFCell score = hssfRow.getCell(3);
+                    user.setUsername(getValue(username));
+                    user.setPassword(getValue(password));
+                    user.setRealname(getValue(realname));
+                    list.add(user);
+                }
+            }
+        }
+        return list;
+    }
+
+    @SuppressWarnings("static-access")
+    private String getValue(HSSFCell hssfCell) {
+        if (hssfCell.getCellType() == hssfCell.CELL_TYPE_BOOLEAN) {
+            // 返回布尔类型的值
+            return String.valueOf(hssfCell.getBooleanCellValue());
+        } else if (hssfCell.getCellType() == hssfCell.CELL_TYPE_NUMERIC) {
+            // 返回数值类型的值
+            return String.valueOf(hssfCell.getNumericCellValue());
+        } else {
+            // 返回字符串类型的值
+            return String.valueOf(hssfCell.getStringCellValue());
+        }
+
+    }
